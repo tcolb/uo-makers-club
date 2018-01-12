a46492065300877938ada0dba276bec3c2430aa1
Tristan Colby <tristan.colb@gmail.com>
2018-01-12 14:03:27 -0800

    added orgsync link
    :

--- index.php
+++ index.php
@@ -65,6 +65,7 @@ include('./includes/base.php'); ?>
             <h4 class="customTitle"><b>Want to join?</b></h4>
             <ul class="customText" style="">
               <li>Club is open to students of any level</li>
+              <li>Register on <a target="_blank" href="https://orgsync.com/169590/chapter">OrgSync</a></li>
               <li>Join our <a target="_blank" href="https://www.facebook.com/groups/871519893028368/">Facebook group</a> and or <a target="_blank" href="https://makerclubuo.slack.com/">Slack channel</a> for updates</li>
               <li style="color: #9c2222">
                 <b><i>Note: you must attend a safety training at the <a target="_blank" href="https://library.uoregon.edu/scilib/psc-dearmond-makerspace" style="text-decoration: underline; color: #500000">makers space</a> before attending meetings</b></i>
