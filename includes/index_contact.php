e3cf9cea094551a937a5befaa7527138119210da
Tristan Colby <tristan.colb@gmail.com>
2018-02-01 16:44:08 -0800

    carousel logic working and updated .gitignore

---
+++ includes/index_contact.php
@@ -0,0 +1,107 @@
+<!-- contact title -->
+<div class="row" id="contact" style="padding-top: 1em;">
+  <div class="col-12 customBlock">
+    <h2 class="customTitle"><b>Contact us</b></h2>
+  </div>
+</div>
+
+<!-- links -->
+<!-- <div class="row">
+  <div class="col-3"></div>
+
+    <div class="col-3">
+      <a target="_blank" href="https://www.facebook.com/groups/871519893028368/">
+        <img src="static/images/facebook.svg" class="img-fluid" style="max-height:50px; min-height:20px"/>
+      </a>
+      <h5 class="customTitle">Facebook Group</h5>
+    </div>
+    <div class="col-3">
+      <a target="_blank" href="https://makerclubuo.slack.com/">
+        <img src="static/images/slack.svg" class="img-fluid" style="max-height:50px; min-height:20px"/>
+      </a>
+      <h5 class="customTitle">Slack Channel</h5>
+    </div>
+
+  <div class="col-3"></div>
+</div> -->
+
+<!-- contactable  -->
+<div class="row">
+  <div class="col-1"></div>
+  <div class="col-lg-10">
+
+    <div class="row">
+      <div class="col-12">
+        <div class="card-deck">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title">Daniel Su</h4>
+              <h5 class="card-title text-muted">Outreach</h5>
+              <hr/>
+              <a href="mailto:dsu@uoregon.edu">dsu@uoregon.edu</a>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title">Kaela Schaefer</h4>
+              <h5 class="card-title text-muted">Co-Chair</h5>
+              <hr/>
+              <a href="mailto:kaelas@uoregon.edu">kaelas@uoregon.edu</a>
+            </div>
+          </div>
+
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title">Michal Young</h4>
+              <h5 class="card-title text-muted">Supporting Faculty Member</h5>
+              <hr/>
+              <a href="mailto:michal@cs.uoregon.edu">michal@cs.uoregon.edu</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="customBlock" id="outreach">
+          <h4 class="customTitle">Outreach</h4>
+          <ul class="list-group">
+            <li class="list-group-item">
+              <h6>Daniel Su</h6>
+              <h6 class="text-muted singleContact">Community/Business</h6>
+            </li>
+            <li class="list-group-item">
+              <h6>Andrew Gao</h6>
+              <h6 class="text-muted singleContact">Robotics</h6>
+            </li>
+            <li class="list-group-item">
+              <h6>Palmer Hogen</h6>
+              <h6 class="text-muted singleContact">Other Universities</h6>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+        <div class="customBlock" id="funding">
+          <h4 class="customTitle">Funding Team</h4>
+          <ul class="list-group">
+            <li class="list-group-item">
+              <h6 class="singleContact">Kaela Schaefer</h6>
+            </li>
+            <li class="list-group-item">
+              <h6 class="singleContact">Jamie Zimmerman</h6>
+            </li>
+            <li class="list-group-item">
+              <h6 class="singleContact">Palmer Hogen</h6>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <div class="col-1"></div>
+</div>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $title = "Index";
 $header = "index_header";
+$disp_meeting = "meeting1";
 include('./includes/base.php'); ?>
 
 <div class="container-fluid">
@@ -27,7 +28,9 @@ include('./includes/base.php'); ?>
     <div class="col-md-10">
 
       <!-- about -->
-      <div class="row" id="about">
+      <div class="rWarning (undo): Buffer ‘*html*’ undo info was 12494703 bytes long.
+The undo info was discarded because it exceeded `undo-outer-limit'.
+ow" id="about">
         <div class="customBlock" style="padding-top: 3em">
           <p class="customText">
             Welcome to the Makers Club at the University of Oregon. Our goal is to strengthen our coding skills while expanding our knowledge about hobbyist electronics design and creation, computer hardware, and prototyping.
@@ -79,10 +82,20 @@ include('./includes/base.php'); ?>
           <h4 class="customTitle"><b>Location and Time</b></h4>
           <ul class="customText" style="">
             <li>The Maker Space in the UO Science Library</li>
-            <li>Meeting are on Thursdays from 5:30PM to 7:00PM</li>
+            <li>Meetings are on Thursdays from 5:30PM to 7:00PM</li>
           </ul>
         </div>
       </div>
+
+
+      <!--
+
+        picture block
+
+      -->
+
+
+      <?php include('./includes/meeting_carousel.php'); ?>
 
 
       <!--
@@ -119,6 +132,7 @@ include('./includes/base.php'); ?>
       </div>
       -->
 
+
       <!--
 
         contact block
@@ -126,113 +140,7 @@ include('./includes/base.php'); ?>
       -->
 
 
-      <!-- contact title -->
-      <div class="row" id="contact" style="padding-top: 1em;">
-        <div class="col-12 customBlock">
-          <h2 class="customTitle"><b>Contact us</b></h2>
-        </div>
-      </div>
-
-      <!-- links -->
-      <!-- <div class="row">
-        <div class="col-3"></div>
-
-          <div class="col-3">
-            <a target="_blank" href="https://www.facebook.com/groups/871519893028368/">
-              <img src="static/images/facebook.svg" class="img-fluid" style="max-height:50px; min-height:20px"/>
-            </a>
-            <h5 class="customTitle">Facebook Group</h5>
-          </div>
-          <div class="col-3">
-            <a target="_blank" href="https://makerclubuo.slack.com/">
-              <img src="static/images/slack.svg" class="img-fluid" style="max-height:50px; min-height:20px"/>
-            </a>
-            <h5 class="customTitle">Slack Channel</h5>
-          </div>
-
-        <div class="col-3"></div>
-      </div> -->
-
-      <!-- contactable  -->
-      <div class="row">
-        <div class="col-1"></div>
-        <div class="col-lg-10">
-
-          <div class="row">
-            <div class="col-12">
-              <div class="card-deck">
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">Daniel Su</h4>
-                    <h5 class="card-title text-muted">Outreach</h5>
-                    <hr/>
-                    <a href="mailto:dsu@uoregon.edu">dsu@uoregon.edu</a>
-                  </div>
-                </div>
-
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">Kaela Schaefer</h4>
-                    <h5 class="card-title text-muted">Co-Chair</h5>
-                    <hr/>
-                    <a href="mailto:kaelas@uoregon.edu">kaelas@uoregon.edu</a>
-                  </div>
-                </div>
-
-                <div class="card">
-                  <div class="card-body">
-                    <h4 class="card-title">Michal Young</h4>
-                    <h5 class="card-title text-muted">Supporting Faculty Member</h5>
-                    <hr/>
-                    <a href="mailto:michal@cs.uoregon.edu">michal@cs.uoregon.edu</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-lg-6">
-              <div class="customBlock" id="outreach">
-                <h4 class="customTitle">Outreach</h4>
-                <ul class="list-group">
-                  <li class="list-group-item">
-                    <h6>Daniel Su</h6>
-                    <h6 class="text-muted singleContact">Community/Business</h6>
-                  </li>
-                  <li class="list-group-item">
-                    <h6>Andrew Gao</h6>
-                    <h6 class="text-muted singleContact">Robotics</h6>
-                  </li>
-                  <li class="list-group-item">
-                    <h6>Palmer Hogen</h6>
-                    <h6 class="text-muted singleContact">Other Universities</h6>
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <div class="col-lg-6">
-              <div class="customBlock" id="funding">
-                <h4 class="customTitle">Funding Team</h4>
-                <ul class="list-group">
-                  <li class="list-group-item">
-                    <h6 class="singleContact">Kaela Schaefer</h6>
-                  </li>
-                  <li class="list-group-item">
-                    <h6 class="singleContact">Jamie Zimmerman</h6>
-                  </li>
-                  <li class="list-group-item">
-                    <h6 class="singleContact">Palmer Hogen</h6>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-        </div>
-        <div class="col-1"></div>
-      </div>
+      <?php include('./includes/index_contact.php'); ?>
 
 
     </div>
@@ -241,4 +149,4 @@ include('./includes/base.php'); ?>
 
 </div>
 
-<?php include('./includes/footer.php') ?>
+<?php include('./includes/footer.php'); ?>
